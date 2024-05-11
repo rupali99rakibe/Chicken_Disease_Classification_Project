ccc35706f96956b55fcb62199657d71c159d6de7
rupali suresh rakibe <rupali99rakibe@gmail.com>
2024-05-11 23:17:06 +0530

    requirements.txt

--- template.py
+++ template.py
@@ -21,7 +21,8 @@ list_of_files=[
     "dvc.yaml",
     "requirements.txt",
     "setup.py",
-    "research/trials.ipynb"
+    "research/trials.ipynb",
+    "templates/index.html"
 ]
 
 for filepath in list_of_files:
